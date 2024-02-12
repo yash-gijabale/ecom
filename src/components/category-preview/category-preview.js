8ba7b40ba37b5692c824d280be024683092aae88
sawantyash11 <sawantyash0011@gmail.com>
2024-02-12 12:16:44 +0530

    spell fixed

--- src/components/category-preview/category-preview.js
+++ src/components/category-preview/category-preview.js
@@ -1,28 +1,28 @@
-import React, {useContext, useEffect, useState} from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { categoryContext } from "../../contexts/category-context";
 import { useParams } from "react-router-dom";
 import SkeletonAnimation from '../../components/skeleton/Skeleton'
 import ProductPreview from "../../components/product-preview/product-preview";
 import CategoryProducts from "./category-product";
 
-const CategoryPreview = () =>{
+const CategoryPreview = () => {
     const param = useParams()
-    const {products} = useContext(categoryContext)
+    const { products } = useContext(categoryContext)
     const [catProduct, setCatProduct] = useState(null)
-    
 
-    useEffect(()=>{
-        const catProductArr = products && products.find((itmes)=>(
-            itmes.title.toLowerCase() ===  param['*']
+
+    useEffect(() => {
+        const catProductArr = products && products.find((itmes) => (
+            itmes.title.toLowerCase() === param['*']
         ))
         setCatProduct(catProductArr)
-    },[param, products])
+    }, [param, products])
 
     console.log(catProduct)
-    return(
+    return (
         <div>
             {
-               catProduct ?  <CategoryProducts productsData = {catProduct} /> : <span>no</span>
+                catProduct ? <CategoryProducts productsData={catProduct} /> : <span>No products</span>
             }
         </div>
     )
